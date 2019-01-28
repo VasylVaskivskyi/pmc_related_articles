d1cddebe9bc7f851a93d08a485ecfb4238bb69df
Yuci Gou <yuci.gou@gmail.com>
2019-01-28 10:07:04 +0000

    Bundle the code

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, './build');
+var APP_DIR = path.resolve(__dirname, './src');
+
+const config = {
+   mode: "development",
+   entry: {
+     main: APP_DIR + '/index.js'
+   },
+   output: {
+     filename: 'bundle.js',
+     path: BUILD_DIR,
+   },
+   module: {
+    rules: [
+     {
+       test: /\.(js)?$/,
+       use: [{
+         loader: "babel-loader",
+         options: {
+           cacheDirectory: true,
+           presets: ['env'] // Transpiles ES2015+
+         }
+       }]
+     }
+    ],
+  }
+};
+
+module.exports = config;
